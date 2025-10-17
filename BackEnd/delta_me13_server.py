0fdd7b89a5521da53505d3c481b57c68adcadd97
Justin_Choi <ka09114321@gmail.com>
2025-10-17 15:46:30 -0600

    updated requirement.txt

--- BackEnd/delta_me13_server.py
+++ BackEnd/delta_me13_server.py
@@ -2,15 +2,20 @@ from fastapi import FastAPI
 from contextlib import asynccontextmanager
 from PythonServer.Global import state
 from PythonServer.pyServerRouters import http_router
+import subprocess
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     # When server starts this code will started first
-    
     state.character_handler = state.CharacterHandler()
+    
     yield # From here, FastAPI's application starts
     
     # This code will be started after server ends
+    subprocess.run(
+                ["taskkill", "/F", "/T", "/PID", str(state.llm_controller.pid)],
+                check=True
+            )
     
 # FastAPI instance
 app = FastAPI(lifespan=lifespan)
